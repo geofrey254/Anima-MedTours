a384713cb8b39d6786c42b88e1215ce0f985ab17
--replace-all <devongeoffreymaina@gmail.com>
2023-03-08 22:52:07 +0300

    Mail change address

--- anima_medtours/settings.py
+++ anima_medtours/settings.py
@@ -63,8 +63,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_SSL_REDIRECT = True
 
 ROOT_URLCONF = 'anima_medtours.urls'
 
